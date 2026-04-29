eeb8c75e2e8bfaee8b88cf4bf3002a6c5d819a00
Liang Mi <github@liangmi.dev>
2026-04-29 20:33:34 +0800

    fix

--- crates/vue_oxlint_jsx/src/parser/codegen.rs
+++ crates/vue_oxlint_jsx/src/parser/codegen.rs
@@ -6,7 +6,7 @@ use crate::parser::ParserImpl;
 impl<'a> ParserImpl<'a> {
   #[inline]
   pub fn jsx_child_text(&self, span: Span, str: &str) -> JSXChild<'a> {
-    let ast_str = if true {
+    let ast_str = if self.config.codegen {
       let bytes = str.as_bytes();
       let mut vec: Vec<u8> = Vec::with_capacity(bytes.len());
       for &b in bytes {
