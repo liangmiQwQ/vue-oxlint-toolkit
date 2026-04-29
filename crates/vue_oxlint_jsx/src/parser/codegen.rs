e778c7a09be38536a4db1d441fdf5ade432e9fda
Liang Mi <github@liangmi.dev>
2026-04-29 17:17:54 +0800

    patch 1

--- crates/vue_oxlint_jsx/src/parser/codegen.rs
+++ crates/vue_oxlint_jsx/src/parser/codegen.rs
@@ -6,21 +6,26 @@ use crate::parser::ParserImpl;
 impl<'a> ParserImpl<'a> {
   #[inline]
   pub fn jsx_child_text(&self, span: Span, str: &str) -> JSXChild<'a> {
-    let bytes = str.as_bytes();
-    let mut vec: Vec<u8> = vec![0; bytes.len()];
-    for &b in bytes {
-      match b {
-        b'&' => vec.extend_from_slice(b"&amp;"),
-        b'<' => vec.extend_from_slice(b"&lt;"),
-        b'>' => vec.extend_from_slice(b"&gt;"),
-        b'{' => vec.extend_from_slice(b"&#123;"),
-        b'}' => vec.extend_from_slice(b"&#125;"),
-        _ => vec.push(b),
+    let ast_str = if true {
+      let bytes = str.as_bytes();
+      let mut vec: Vec<u8> = Vec::with_capacity(bytes.len());
+      for &b in bytes {
+        match b {
+          b'&' => vec.extend_from_slice(b"&amp;"),
+          b'<' => vec.extend_from_slice(b"&lt;"),
+          b'>' => vec.extend_from_slice(b"&gt;"),
+          b'{' => vec.extend_from_slice(b"&#123;"),
+          b'}' => vec.extend_from_slice(b"&#125;"),
+          _ => vec.push(b),
+        }
       }
-    }
-    let str = unsafe { str::from_utf8_unchecked(&vec) };
+      let escaped = unsafe { str::from_utf8_unchecked(&vec) };
+      assert_eq!(escaped, str);
+      self.ast.str(escaped)
+    } else {
+      self.ast.str(str)
+    };
 
-    let ast_str = self.ast.str(str);
     self.ast.jsx_child_text(span, ast_str, Some(ast_str))
   }
 }
