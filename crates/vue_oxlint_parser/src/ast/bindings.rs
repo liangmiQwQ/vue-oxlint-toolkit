d4bee8f0d8bb835ac37ec74902ed8da9f326dbdf
Liang Mi <github@liangmi.dev>
2026-05-04 14:20:08 +0800

    refactor(parser): use Borrow for bindings

--- crates/vue_oxlint_parser/src/ast/bindings.rs
+++ crates/vue_oxlint_parser/src/ast/bindings.rs
@@ -1,20 +1,18 @@
 //! Structs defined in this mod aren't V* nodes, it is just a helper struct to store binding-related things.
 //!
 //! For these structs, we should always use `'b` lifetime and Box, to avoid cloning nodes
-
-use oxc_allocator::Box;
 use oxc_ast::ast::IdentifierReference;
 use oxc_estree::{ESTree, StructSerializer};
 
 #[derive(Debug)]
 pub struct Reference<'b> {
-  pub id: Box<'b, IdentifierReference<'b>>,
+  pub id: &'b IdentifierReference<'b>,
   pub mode: &'static str,
 }
 
 #[derive(Debug)]
 pub struct Variable<'b> {
-  pub id: Box<'b, IdentifierReference<'b>>,
+  pub id: &'b IdentifierReference<'b>,
   pub kind: &'static str,
 }
 
